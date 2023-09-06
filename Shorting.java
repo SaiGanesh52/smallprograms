f88d6c2272bcd00a12f18a43b6296666193ca001
SaiGanesh52 <142702206+SaiGanesh52@users.noreply.github.com>
2023-09-05 14:05:32 -1200

    Add files via upload

---
+++ Shorting.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Shorting
+{
+public static void main(String args[])
+{
+int arr[]={43,6,33,56,8,26,13,90};
+Arrays.sort(arr);
+System.out.println("after sortng the arrays :"+Arrays.toString(arr));
+}
+}

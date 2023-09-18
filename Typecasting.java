0361a6f1b57b693765c2cbe3949b85c37df50c3b
SaiGanesh52 <142702206+SaiGanesh52@users.noreply.github.com>
2023-09-17 13:19:13 -1200

    Add files via upload

---
+++ Typecasting.java
@@ -0,0 +1,14 @@
+//implicit type casting byte->short->int->long->float->double
+class Typecasting
+{
+public static void main(String args[])
+{
+   int i=333;
+   long d=i;
+   float f=d;
+System.out.println("integer value is"+i);
+System.out.println("long value is"+d);
+System.out.println("float value is"+f);
+
+}
+}
